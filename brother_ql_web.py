57441059f352681915380cdfd6edfe66072ad7ad
Mike Trebilcock <m.trebilcock@htempest.co.uk>
2020-05-13 14:15:10 +0100

    Make Title Bold

--- brother_ql_web.py
+++ brother_ql_web.py
@@ -260,117 +260,132 @@ def print_label(data):
     try:
         logger.debug("recieved data: {}".format(data))
         font_path = FONTS['DejaVu Sans Mono']['Book']
-        im_font = ImageFont.truetype(font_path, 32)
-        width = 696
-        height = 230
-        im = Image.new('RGB', (width, height), 'white')
-        draw = ImageDraw.Draw(im)
-
+        title_font_path = FONTS['DejaVu Sans Mono']['Bold']
+        title_font = ImageFont.truetype(title_font_path, 32)
+        im_font = ImageFont.truetype(font_path, 30)
+        
         lines = []
-        lines.append(data['testForName'])
         for line in data['homeAddress'].split('\n'):
             if line == '': line = ' '
             lines.append(line)
         
         lines.append(data['postcode'])
-        lines.append("TEL: {}".format(data['postcode']))
+        lines.append("TEL: {}".format(data['phone']))
         lines.append("DOB: {}".format(data['dob']))
         lines.append("TEST DATE: {}".format(data['appointmentDate']))
         text = '\n'.join(lines)
 
+        im = Image.new('L', (20, 20), 'white')
+        draw = ImageDraw.Draw(im)
+
+        title_text_size = draw.multiline_textsize(data['testForName'], font=title_font)
+        body_text_size = draw.multiline_textsize(text, font=im_font)
+
+        width = 696
+        height = body_text_size[1] + title_text_size[1]
+        im = Image.new('RGB', (width, height), 'white')
+        draw = ImageDraw.Draw(im)
+
+
         offset = 0, 0
         color = (0, 0, 0)
+        draw.multiline_text(offset, data['testForName'], color, title_font, 'left')
+        offset = 0, title_text_size
         draw.multiline_text(offset, text, color, im_font, 'left')
         im.save('sample-out.png')
     except Exception as e:
         logger.error(e)
 
 def main():
-    global DEBUG, FONTS, BACKEND_CLASS, CONFIG
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--port', default=False)
-    parser.add_argument('--loglevel', type=lambda x: getattr(logging, x.upper()), default=False)
-    parser.add_argument('--font-folder', default=False, help='folder for additional .ttf/.otf fonts')
-    parser.add_argument('--default-label-size', default=False, help='Label size inserted in your printer. Defaults to 62.')
-    parser.add_argument('--default-orientation', default=False, choices=('standard', 'rotated'), help='Label orientation, defaults to "standard". To turn your text by 90°, state "rotated".')
-    parser.add_argument('--model', default=False, choices=models, help='The model of your printer (default: QL-500)')
-    parser.add_argument('printer',  nargs='?', default=False, help='String descriptor for the printer to use (like tcp://192.168.0.23:9100 or file:///dev/usb/lp0)')
-    args = parser.parse_args()
-
-    if args.printer:
-        CONFIG['PRINTER']['PRINTER'] = args.printer
-
-    if args.port:
-        PORT = args.port
-    else:
-        PORT = CONFIG['SERVER']['PORT']
-
-    if args.loglevel:
-        LOGLEVEL = args.loglevel
-    else:
-        LOGLEVEL = CONFIG['SERVER']['LOGLEVEL']
-
-    if LOGLEVEL == 'DEBUG':
-        DEBUG = True
-    else:
-        DEBUG = False
-
-    if args.model:
-        CONFIG['PRINTER']['MODEL'] = args.model
-
-    if args.default_label_size:
-        CONFIG['LABEL']['DEFAULT_SIZE'] = args.default_label_size
-
-    if args.default_orientation:
-        CONFIG['LABEL']['DEFAULT_ORIENTATION'] = args.default_orientation
-
-    if args.font_folder:
-        ADDITIONAL_FONT_FOLDER = args.font_folder
-    else:
-        ADDITIONAL_FONT_FOLDER = CONFIG['SERVER']['ADDITIONAL_FONT_FOLDER']
-
-
-    logging.basicConfig(level=LOGLEVEL)
-
     try:
-        selected_backend = guess_backend(CONFIG['PRINTER']['PRINTER'])
-    except ValueError:
-        parser.error("Couln't guess the backend to use from the printer string descriptor")
-    BACKEND_CLASS = backend_factory(selected_backend)['backend_class']
+        global DEBUG, FONTS, BACKEND_CLASS, CONFIG
+        parser = argparse.ArgumentParser(description=__doc__)
+        parser.add_argument('--port', default=False)
+        parser.add_argument('--loglevel', type=lambda x: getattr(logging, x.upper()), default=False)
+        parser.add_argument('--font-folder', default=False, help='folder for additional .ttf/.otf fonts')
+        parser.add_argument('--default-label-size', default=False, help='Label size inserted in your printer. Defaults to 62.')
+        parser.add_argument('--default-orientation', default=False, choices=('standard', 'rotated'), help='Label orientation, defaults to "standard". To turn your text by 90°, state "rotated".')
+        parser.add_argument('--model', default=False, choices=models, help='The model of your printer (default: QL-500)')
+        parser.add_argument('printer',  nargs='?', default=False, help='String descriptor for the printer to use (like tcp://192.168.0.23:9100 or file:///dev/usb/lp0)')
+        args = parser.parse_args()
 
-    if CONFIG['LABEL']['DEFAULT_SIZE'] not in label_sizes:
-        parser.error("Invalid --default-label-size. Please choose on of the following:\n:" + " ".join(label_sizes))
+        if args.printer:
+            CONFIG['PRINTER']['PRINTER'] = args.printer
 
-    FONTS = get_fonts()
-    if ADDITIONAL_FONT_FOLDER:
-        FONTS.update(get_fonts(ADDITIONAL_FONT_FOLDER))
+        if args.port:
+            PORT = args.port
+        else:
+            PORT = CONFIG['SERVER']['PORT']
 
-    if not FONTS:
-        sys.stderr.write("Not a single font was found on your system. Please install some or use the \"--font-folder\" argument.\n")
-        sys.exit(2)
+        if args.loglevel:
+            LOGLEVEL = args.loglevel
+        else:
+            LOGLEVEL = CONFIG['SERVER']['LOGLEVEL']
 
-    for font in CONFIG['LABEL']['DEFAULT_FONTS']:
+        if LOGLEVEL == 'DEBUG':
+            DEBUG = True
+        else:
+            DEBUG = False
+
+        if args.model:
+            CONFIG['PRINTER']['MODEL'] = args.model
+
+        if args.default_label_size:
+            CONFIG['LABEL']['DEFAULT_SIZE'] = args.default_label_size
+
+        if args.default_orientation:
+            CONFIG['LABEL']['DEFAULT_ORIENTATION'] = args.default_orientation
+
+        if args.font_folder:
+            ADDITIONAL_FONT_FOLDER = args.font_folder
+        else:
+            ADDITIONAL_FONT_FOLDER = CONFIG['SERVER']['ADDITIONAL_FONT_FOLDER']
+
+
+        logging.basicConfig(level=LOGLEVEL)
+
         try:
-            FONTS[font['family']][font['style']]
-            CONFIG['LABEL']['DEFAULT_FONTS'] = font
-            logger.debug("Selected the following default font: {}".format(font))
-            break
-        except: pass
-    if CONFIG['LABEL']['DEFAULT_FONTS'] is None:
-        sys.stderr.write('Could not find any of the default fonts. Choosing a random one.\n')
-        family =  random.choice(list(FONTS.keys()))
-        style =   random.choice(list(FONTS[family].keys()))
-        CONFIG['LABEL']['DEFAULT_FONTS'] = {'family': family, 'style': style}
-        sys.stderr.write('The default font is now set to: {family} ({style})\n'.format(**CONFIG['LABEL']['DEFAULT_FONTS']))
+            selected_backend = guess_backend(CONFIG['PRINTER']['PRINTER'])
+        except ValueError:
+            parser.error("Couln't guess the backend to use from the printer string descriptor")
+        BACKEND_CLASS = backend_factory(selected_backend)['backend_class']
 
-    serial = get_serial()
-    version = git_version()
-    branch = git_branch()
-    firestore = Firestore(serial, branch, version)
+        if CONFIG['LABEL']['DEFAULT_SIZE'] not in label_sizes:
+            parser.error("Invalid --default-label-size. Please choose on of the following:\n:" + " ".join(label_sizes))
 
-    firestore.listen(print_label)
+        FONTS = get_fonts()
+        if ADDITIONAL_FONT_FOLDER:
+            FONTS.update(get_fonts(ADDITIONAL_FONT_FOLDER))
 
-    run(host=CONFIG['SERVER']['HOST'], port=PORT, debug=DEBUG)
+        if not FONTS:
+            sys.stderr.write("Not a single font was found on your system. Please install some or use the \"--font-folder\" argument.\n")
+            sys.exit(2)
+
+        for font in CONFIG['LABEL']['DEFAULT_FONTS']:
+            try:
+                FONTS[font['family']][font['style']]
+                CONFIG['LABEL']['DEFAULT_FONTS'] = font
+                logger.debug("Selected the following default font: {}".format(font))
+                break
+            except: pass
+        if CONFIG['LABEL']['DEFAULT_FONTS'] is None:
+            sys.stderr.write('Could not find any of the default fonts. Choosing a random one.\n')
+            family =  random.choice(list(FONTS.keys()))
+            style =   random.choice(list(FONTS[family].keys()))
+            CONFIG['LABEL']['DEFAULT_FONTS'] = {'family': family, 'style': style}
+            sys.stderr.write('The default font is now set to: {family} ({style})\n'.format(**CONFIG['LABEL']['DEFAULT_FONTS']))
+
+        serial = get_serial()
+        version = git_version()
+        branch = git_branch()
+        firestore = Firestore(serial, branch, version)
+
+        firestore.listen(print_label)
+
+        run(host=CONFIG['SERVER']['HOST'], port=PORT, debug=DEBUG)
+
+    except KeyboardInterrupt:
+        firestore.close()
 
 
 if __name__ == "__main__":
