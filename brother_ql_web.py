62e6ef4f467c6d68f8580aae77f3d32b41db0f9e
Mike Trebilcock <m.trebilcock@htempest.co.uk>
2020-05-21 10:56:57 +0100

    improve error handling

--- brother_ql_web.py
+++ brother_ql_web.py
@@ -38,195 +38,6 @@ except FileNotFoundError as e:
     with open('config.example.json', encoding='utf-8') as fh:
         CONFIG = json.load(fh)
 
-
-@route('/')
-def index():
-    redirect('/labeldesigner')
-
-@route('/static/<filename:path>')
-def serve_static(filename):
-    return static_file(filename, root='./static')
-
-@route('/labeldesigner')
-@view('labeldesigner.jinja2')
-def labeldesigner():
-    font_family_names = sorted(list(FONTS.keys()))
-    return {'font_family_names': font_family_names,
-            'fonts': FONTS,
-            'label_sizes': LABEL_SIZES,
-            'website': CONFIG['WEBSITE'],
-            'label': CONFIG['LABEL']}
-
-def get_label_context(request):
-    """ might raise LookupError() """
-
-    d = request.params.decode() # UTF-8 decoded form data
-
-    font_family = d.get('font_family').rpartition('(')[0].strip()
-    font_style  = d.get('font_family').rpartition('(')[2].rstrip(')')
-    context = {
-      'text':          d.get('text', None),
-      'font_size': int(d.get('font_size', 100)),
-      'font_family':   font_family,
-      'font_style':    font_style,
-      'label_size':    d.get('label_size', "62"),
-      'kind':          label_type_specs[d.get('label_size', "62")]['kind'],
-      'margin':    int(d.get('margin', 10)),
-      'threshold': int(d.get('threshold', 70)),
-      'align':         d.get('align', 'center'),
-      'orientation':   d.get('orientation', 'standard'),
-      'margin_top':    float(d.get('margin_top',    24))/100.,
-      'margin_bottom': float(d.get('margin_bottom', 45))/100.,
-      'margin_left':   float(d.get('margin_left',   35))/100.,
-      'margin_right':  float(d.get('margin_right',  35))/100.,
-    }
-    context['margin_top']    = int(context['font_size']*context['margin_top'])
-    context['margin_bottom'] = int(context['font_size']*context['margin_bottom'])
-    context['margin_left']   = int(context['font_size']*context['margin_left'])
-    context['margin_right']  = int(context['font_size']*context['margin_right'])
-
-    context['fill_color']  = (255, 0, 0) if 'red' in context['label_size'] else (0, 0, 0)
-
-    def get_font_path(font_family_name, font_style_name):
-        try:
-            if font_family_name is None or font_style_name is None:
-                font_family_name = CONFIG['LABEL']['DEFAULT_FONTS']['family']
-                font_style_name =  CONFIG['LABEL']['DEFAULT_FONTS']['style']
-            font_path = FONTS[font_family_name][font_style_name]
-        except KeyError:
-            raise LookupError("Couln't find the font & style")
-        return font_path
-
-    context['font_path'] = get_font_path(context['font_family'], context['font_style'])
-
-    def get_label_dimensions(label_size):
-        try:
-            ls = label_type_specs[context['label_size']]
-        except KeyError:
-            raise LookupError("Unknown label_size")
-        return ls['dots_printable']
-
-    width, height = get_label_dimensions(context['label_size'])
-    if height > width: width, height = height, width
-    if context['orientation'] == 'rotated': height, width = width, height
-    context['width'], context['height'] = width, height
-
-    return context
-
-def create_label_im(text, **kwargs):
-    label_type = kwargs['kind']
-    im_font = ImageFont.truetype(kwargs['font_path'], kwargs['font_size'])
-    im = Image.new('L', (20, 20), 'white')
-    draw = ImageDraw.Draw(im)
-    # workaround for a bug in multiline_textsize()
-    # when there are empty lines in the text:
-    lines = []
-    for line in text.split('\n'):
-        if line == '': line = ' '
-        lines.append(line)
-    text = '\n'.join(lines)
-    linesize = im_font.getsize(text)
-    textsize = draw.multiline_textsize(text, font=im_font)
-    width, height = kwargs['width'], kwargs['height']
-    if kwargs['orientation'] == 'standard':
-        if label_type in (ENDLESS_LABEL,):
-            height = textsize[1] + kwargs['margin_top'] + kwargs['margin_bottom']
-    elif kwargs['orientation'] == 'rotated':
-        if label_type in (ENDLESS_LABEL,):
-            width = textsize[0] + kwargs['margin_left'] + kwargs['margin_right']
-    im = Image.new('RGB', (width, height), 'white')
-    draw = ImageDraw.Draw(im)
-    if kwargs['orientation'] == 'standard':
-        if label_type in (DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL):
-            vertical_offset  = (height - textsize[1])//2
-            vertical_offset += (kwargs['margin_top'] - kwargs['margin_bottom'])//2
-        else:
-            vertical_offset = kwargs['margin_top']
-        horizontal_offset = max((width - textsize[0])//2, 0)
-    elif kwargs['orientation'] == 'rotated':
-        vertical_offset  = (height - textsize[1])//2
-        vertical_offset += (kwargs['margin_top'] - kwargs['margin_bottom'])//2
-        if label_type in (DIE_CUT_LABEL, ROUND_DIE_CUT_LABEL):
-            horizontal_offset = max((width - textsize[0])//2, 0)
-        else:
-            horizontal_offset = kwargs['margin_left']
-    offset = horizontal_offset, vertical_offset
-    draw.multiline_text(offset, text, kwargs['fill_color'], font=im_font, align=kwargs['align'])
-    return im
-
-@get('/api/preview/text')
-@post('/api/preview/text')
-def get_preview_image():
-    context = get_label_context(request)
-    im = create_label_im(**context)
-    return_format = request.query.get('return_format', 'png')
-    if return_format == 'base64':
-        import base64
-        response.set_header('Content-type', 'text/plain')
-        return base64.b64encode(image_to_png_bytes(im))
-    else:
-        response.set_header('Content-type', 'image/png')
-        return image_to_png_bytes(im)
-
-def image_to_png_bytes(im):
-    image_buffer = BytesIO()
-    im.save(image_buffer, format="PNG")
-    image_buffer.seek(0)
-    return image_buffer.read()
-
-@post('/api/print/text')
-@get('/api/print/text')
-def print_text():
-    """
-    API to print a label
-
-    returns: JSON
-
-    Ideas for additional URL parameters:
-    - alignment
-    """
-
-    return_dict = {'success': False}
-
-    try:
-        context = get_label_context(request)
-    except LookupError as e:
-        return_dict['error'] = e.msg
-        return return_dict
-
-    if context['text'] is None:
-        return_dict['error'] = 'Please provide the text for the label'
-        return return_dict
-
-    im = create_label_im(**context)
-    if DEBUG: im.save('sample-out.png')
-
-    if context['kind'] == ENDLESS_LABEL:
-        rotate = 0 if context['orientation'] == 'standard' else 90
-    elif context['kind'] in (ROUND_DIE_CUT_LABEL, DIE_CUT_LABEL):
-        rotate = 'auto'
-
-    qlr = BrotherQLRaster(CONFIG['PRINTER']['MODEL'])
-    red = False
-    if 'red' in context['label_size']:
-        red = True
-    create_label(qlr, im, context['label_size'], red=red, threshold=context['threshold'], cut=True, rotate=rotate)
-
-    if not DEBUG:
-        try:
-            be = BACKEND_CLASS(CONFIG['PRINTER']['PRINTER'])
-            be.write(qlr.data)
-            be.dispose()
-            del be
-        except Exception as e:
-            return_dict['message'] = str(e)
-            logger.warning('Exception happened: %s', e)
-            return return_dict
-
-    return_dict['success'] = True
-    if DEBUG: return_dict['data'] = str(qlr.data)
-    return return_dict
-
 def git_version():
     gitproc = Popen(['git', 'rev-parse', 'HEAD'],
                     stdout=PIPE)
@@ -407,13 +218,25 @@ def main():
         serial = get_serial()
         version = git_version()
         branch = git_branch()
-        firestore = Firestore(serial, branch, version)
-
-        firestore.listen(print_label)
+        firestore = None
+        while firestore is None:
+            try
+                firestore = Firestore(serial, branch, version)
+                firestore.listen(print_label)
+            except Exception as e:
+                logger.error(e)
 
         while True:
-            firestore.ping()
-            time.sleep(5)
+            try:
+                firestore.ping()
+                time.sleep(5)
+            except Exception as e:
+                logger.error(e)
+                try:
+                    firestore.close()
+                 except: pass
+                firestore = Firestore(serial, branch, version)
+                firestore.listen(print_label)
         #run(host=CONFIG['SERVER']['HOST'], port=PORT, debug=DEBUG)
 
     except KeyboardInterrupt:
